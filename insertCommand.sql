dad5995d3bc03131824bfc1e9e70ace1015507af
Savely Volodkov <s.volodkov@syberry.com>
2023-03-26 18:09:45 +0300

    task 4 completed

---
+++ insertCommand.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION PrintInserCommant (insertionId IN NUMBER) RETURN VARCHAR2 IS
+    result VARCHAR2;
+    isExist NUMBER;
+BEGIN
+    SELECT COUNT(id) INTO isExist FROM MyTable WHERE id = insertionId;
+    
+    IF isExist = 1 THEN
+         return ('id=' || TO_CHAR(new_id) || 
+                    ' is already exist in MyTable');
+    ELSE    
+        result := 'INSERT INTO MyTable VALUES(' || TO_CHAR(new_id) || ', someValue' ');';
+    END IF;
+    
+    RETURN result;
+END PrintInserCommant;
