77c64855e626870fbbb23cecee88f177bf0cb3a5
Savely Volodkov <s.volodkov@syberry.com>
2023-03-26 22:39:29 +0300

    Refactoring

--- fuctionOddEven.sql
+++ fuctionOddEven.sql
@@ -12,19 +12,19 @@ BEGIN
     
     if odd = even
     then
-    -- dbms_output.put_line('EQUAL');
+    dbms_output.put_line('EQUAL');
     return 'EQUAL';
     end if;
     
     if odd > even
     then
-    -- dbms_output.put_line('FALSE');
+    dbms_output.put_line('FALSE');
     return 'FALSE';
     end if;
     
     if even > odd
     then
-    -- dbms_output.put_line('TRUE');
+    dbms_output.put_line('TRUE');
     return 'TRUE';
     end if;
 END;
